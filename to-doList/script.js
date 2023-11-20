cc0280ab12f13ff7f4bd2420bcbf8e514b638c76
xDanyV <danyvalencia0505@hotmail.com>
2023-11-20 13:29:41 -0800

    Agregue Boton de eliminar

--- to-doList/script.js
+++ to-doList/script.js
@@ -5,8 +5,8 @@ let Tareas = [];
 document.addEventListener("DOMContentLoaded", function () {
     if (localStorage.getItem("tareas")) {
         Tareas = JSON.parse(localStorage.getItem("tareas"));
-        Tareas.forEach(function(tarea){
-            agregarFromStorage(tarea.texto,tarea.completada);
+        Tareas.forEach(function (tarea) {
+            agregarFromStorage(tarea.texto, tarea.completada);
         });
     }
 });
@@ -19,27 +19,38 @@ btnAgregar.onclick = function () {
 
 const btnDeleteCache = document.getElementById("btnDeleteCache");
 
-btnDeleteCache.onclick = function(){
+btnDeleteCache.onclick = function () {
     localStorage.removeItem("tareas");
     location.reload(true);
 }
 
-function agregarFromStorage(tarea,completada) {
+function agregarFromStorage(tarea, completada) {
     let li = document.createElement("li");
 
     li.innerHTML = tarea;
     li.classList.add("list-group-item");
 
-    
+
     if (completada) {
-        li.classList.add("tachado");
+        li.classList.add("tachado", "list-group-item-success");
     }
 
     li.onclick = function () {
         li.classList.toggle("tachado");
+        li.classList.toggle("list-group-item-success");
 
         completada = !completada;
         actualizarTarea(tarea, completada);
+    }
+
+    let btnDelete = document.createElement("button");
+    btnDelete.innerHTML = "X"
+    btnDelete.classList.add("btn", "btn-danger");
+    li.appendChild(btnDelete);
+
+    btnDelete.onclick = function(){
+        eliminarTarea(tarea);
+        li.remove();
     }
 
     tablaTareas.appendChild(li);
@@ -48,11 +59,11 @@ function agregarFromStorage(tarea,completada) {
 
 function agregarTarea() {
     let Tarea = document.getElementById("txtTarea").value;
-    
+
     if (Tarea == "") {
         alert("Agrega una tarea");
     } else {
-        Tareas.push({texto: Tarea, completada: false});
+        Tareas.push({ texto: Tarea, completada: false });
         let li = document.createElement("li");
 
         li.innerHTML = Tarea;
@@ -60,7 +71,18 @@ function agregarTarea() {
 
         li.onclick = function () {
             li.classList.toggle("tachado");
+            li.classList.toggle("list-group-item-success");
             actualizarTarea(Tarea);
+        }
+
+        let btnDelete = document.createElement("button");
+        btnDelete.innerHTML = "X"
+        btnDelete.classList.add("btn", "btn-danger");
+        li.appendChild(btnDelete);
+
+        btnDelete.onclick = function(){
+            eliminarTarea(Tarea);
+            li.remove();
         }
 
         tablaTareas.appendChild(li);
@@ -71,12 +93,20 @@ function agregarTarea() {
     }
 }
 
-function actualizarTarea(texto){
-    Tareas.forEach(function(item){
-        if(item.texto == texto){
+function actualizarTarea(texto) {
+    Tareas.forEach(function (item) {
+        if (item.texto == texto) {
             item.completada = !item.completada;
             localStorage.setItem("tareas", JSON.stringify(Tareas));
             return;
         }
     });
+}
+
+function eliminarTarea(texto){
+    Tareas = Tareas.filter(function (item) {
+        return item.texto !== texto;
+    });
+
+    localStorage.setItem("tareas", JSON.stringify(Tareas));
 }
