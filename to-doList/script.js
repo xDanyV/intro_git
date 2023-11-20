13a80b78c271d9b44caab3684a61867c695f07e0
xDanyV <danyvalencia0505@hotmail.com>
2023-11-20 12:58:29 -0800

    Mejorado el guardado en cache

--- to-doList/script.js
+++ to-doList/script.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (localStorage.getItem("tareas")) {
         Tareas = JSON.parse(localStorage.getItem("tareas"));
         Tareas.forEach(function(tarea){
-            agregarFromStorage(tarea);
+            agregarFromStorage(tarea.texto,tarea.completada);
         });
     }
 });
@@ -24,14 +24,22 @@ btnDeleteCache.onclick = function(){
     location.reload(true);
 }
 
-function agregarFromStorage(tarea) {
+function agregarFromStorage(tarea,completada) {
     let li = document.createElement("li");
 
     li.innerHTML = tarea;
     li.classList.add("list-group-item");
 
+    
+    if (completada) {
+        li.classList.add("tachado");
+    }
+
     li.onclick = function () {
         li.classList.toggle("tachado");
+
+        completada = !completada;
+        actualizarTarea(tarea, completada);
     }
 
     tablaTareas.appendChild(li);
@@ -44,7 +52,7 @@ function agregarTarea() {
     if (Tarea == "") {
         alert("Agrega una tarea");
     } else {
-        Tareas.push(Tarea);
+        Tareas.push({texto: Tarea, completada: false});
         let li = document.createElement("li");
 
         li.innerHTML = Tarea;
@@ -52,6 +60,7 @@ function agregarTarea() {
 
         li.onclick = function () {
             li.classList.toggle("tachado");
+            actualizarTarea(Tarea);
         }
 
         tablaTareas.appendChild(li);
@@ -60,4 +69,14 @@ function agregarTarea() {
         localStorage.setItem("tareas", JSON.stringify(Tareas));
 
     }
+}
+
+function actualizarTarea(texto){
+    Tareas.forEach(function(item){
+        if(item.texto == texto){
+            item.completada = !item.completada;
+            localStorage.setItem("tareas", JSON.stringify(Tareas));
+            return;
+        }
+    });
 }
